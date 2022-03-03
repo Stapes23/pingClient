6b113ffd36075f42189a18f3122b4aaa813784d3
Stapes23 <60237825+Stapes23@users.noreply.github.com>
2022-03-03 08:07:36 -0500

    Initial commit, just notes and kind of backbone of pingCLient

---
+++ main.py
@@ -0,0 +1,29 @@
+# Your ping client program should sends 10 ping requests to the server. After sending
+#each request client waits up to one second for reply from the server. If no reply is received
+#during this time period, client times out. In this case you should print out the time out message.
+#Specifically, your client program should do the following:
+#(1)Print out the current time and the ping attempt number. Send the ping message using UDP.
+#(2) Print the response message from server, if any (the server will capitalize the message sent
+#by the client and send it back to the client). Print out the elapsed time in microseconds.
+#(3) Calculate and print the round trip time (RTT), in microseconds, of each packet, if server
+#responses.(4) Otherwise, print “Request timed out
+
+for i in range(10):
+    if true: ##(1)Print out the current time and the ping attempt number. Send the ping message using UDP.
+         # (2) Print the response message from server, if any (the server will capitalize the message sent
+            # by the client and send it back to the client). Print out the elapsed time in microseconds.
+            # (3) Calculate and print the round trip time (RTT), in microseconds, of each packet, if server
+                # responses.
+#
+else:
+    print ("request timed out, please try again")
+# (4) Otherwise, print “Request timed out
+
+def print_hi(name):
+
+
+
+if __name__ == '__main__':
+   
+
+
